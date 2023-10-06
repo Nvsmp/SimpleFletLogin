042acddc324b8b3de62482867207b8d1324eb0bd
Nvsmp <oldblack26@gmail.com>
2023-10-06 15:23:48 -0300

    login

--- main.py
+++ main.py
@@ -15,8 +15,28 @@ def main(pagina:Page):
 
     def logar(evento):
         if tf_login.value != '' and tf_upswd.value != '':
-            resp_serv = requests.get(f"{os.environ['link_api_login']}{tf_login.value},{tf_upswd.value}")
-            print(resp_serv)
+            tf_login.read_only = True
+            tf_upswd.read_only = True
+            eb_confirmar_login.disabled = True
+            pagina.update()
+            resp_serv = ''
+            for itens in requests.get(f"{os.environ['link_api_login']}{tf_login.value},{tf_upswd.value}"):
+                resp_serv = f"{resp_serv}{itens.decode('utf-8')}"
+            resp_serv = eval(resp_serv)
+            if list(resp_serv)[0] != "" and resp_serv.get(os.environ['b3']) == tf_upswd.value:
+                print(f"Login Autorizado\nLogin:{ resp_serv.get(os.environ['b1']) }\nEmail:{ resp_serv.get(os.environ['b2']) }\nSenha:{resp_serv.get(os.environ['b3'])}")
+                tela_sucesso_login()
+                ############## 2 FACTOR #####################
+            else:
+                tf_login.read_only = False
+                tf_upswd.read_only = False
+                eb_confirmar_login.disabled = False
+                pagina.update()
+                
+    def tela_sucesso_login():
+        pagina.remove(col_login)
+        pagina.add(txt_logado)
+        pagina.update()
 
     pagina.vertical_alignment = ft.MainAxisAlignment.CENTER
     pagina.horizontal_alignment = ft.CrossAxisAlignment.CENTER
@@ -36,6 +56,8 @@ def main(pagina:Page):
     eb_confirmar_login = ft.ElevatedButton(text='LOGAR', on_click=logar)
     linha_btn_login = ft.Row([eb_home,eb_confirmar_login],alignment=ft.MainAxisAlignment.CENTER)
     col_login = ft.Column(controls=[tf_login,tf_upswd, linha_btn_login], horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+    #PAG LOGADO
+    txt_logado = ft.Text(value='LOGADO COM SUCESSO')
 
     linha_bemvindo = ft.Row([txt_bemvindo], alignment=ft.MainAxisAlignment.CENTER, spacing=1000)
     linha_bts = ft.Row([eb_login,eb_registrar],alignment=ft.MainAxisAlignment.CENTER, spacing=10)
