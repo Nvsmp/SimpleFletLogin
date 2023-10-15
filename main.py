347f1332c94780dc278ea5b46a6bd46646065e3c
Nvsmp <oldblack26@gmail.com>
2023-10-15 15:49:16 -0300

    2F login, tela de cadastro OK

--- main.py
+++ main.py
@@ -22,31 +22,49 @@ def main(pagina:Page):
             pagina.update()
             resp_serv = ''
             r = requests.get(f"{os.environ['link_api_login']}{tf_elogin.value},{tf_upswd.value}")
-            print(r)
-            for itens in r:
-                resp_serv = f"{resp_serv}{itens.decode('utf-8')}"
-            resp_serv = eval(resp_serv)
-            rs = list(resp_serv)[0]
-            print(rs)
-            if rs != "" and resp_serv.get(os.environ['b3']) == tf_upswd.value:
-                print(f"Login Autorizado\nLogin:{ resp_serv.get(os.environ['b1']) }\nEmail:{ resp_serv.get(os.environ['b2']) }\nSenha:{resp_serv.get(os.environ['b3'])}")
-                ############## 2 FACTOR #####################
-                pagina.dialog = pop_up_2fl
-                pop_up_2fl.open = True
-                pagina.update()
-            elif rs == "":
-                print("DEU RUIM NO SERV KKK")
-                pagina.dialog = pop_up_erroserv
-                pop_up_erroserv.open = True
-                tf_elogin.read_only = False
-                tf_upswd.read_only = False
-                eb_confirmar_login.disabled = False
-                pagina.update()
+            if r.status_code == 200:
+                for itens in r:
+                    resp_serv = f"{resp_serv}{itens.decode('utf-8')}"
+                resp_serv = eval(resp_serv)
+                rs = list(resp_serv)[0]
+                print(rs)
+                if rs != "" and resp_serv.get(os.environ['b3']) == tf_upswd.value:
+                    #emailC = resp_serv.get(os.environ['b2'])
+                    #print(f"Login Autorizado\nLogin:{ resp_serv.get(os.environ['b1']) }\nEmail:{emailC}\nSenha:{resp_serv.get(os.environ['b3'])}")
+
+                    ############## 2 FACTOR #####################
+                    pagina.dialog = pop_up_2fl
+                    r2fr = requests.get(f"{os.environ['b7']}{tf_elogin.value}")
+                    pop_up_2fl.open = True
+                    pagina.update()
+                elif rs == "":
+                    print("LOGIN NEGADO")
+                    pop_up_erroserv.title = ft.Text('DADOS NAO CONFEREM')
+                    pagina.dialog = pop_up_erroserv
+                    pop_up_erroserv.open = True
+                    tf_elogin.read_only = False
+                    tf_upswd.read_only = False
+                    eb_confirmar_login.disabled = False
+                    pagina.update()
+                elif eval(r.text).get('erro') == "bd":
+                    print("BD OFF")
+                    pop_up_erroserv.title = ft.Text('BD OFF')
+                    pagina.dialog = pop_up_erroserv
+                    pop_up_erroserv.open = True
+                    tf_elogin.read_only = False
+                    tf_upswd.read_only = False
+                    eb_confirmar_login.disabled = False
+                    pagina.update()
+                else:
+                    pop_up_erroserv.title = ft.Text('?')
+                    pagina.dialog = pop_up_erroserv
+                    pop_up_erroserv.open = True
+                    tf_elogin.read_only = False
+                    tf_upswd.read_only = False
+                    eb_confirmar_login.disabled = False
+                    pagina.update()
             else:
-                tf_elogin.read_only = False
-                tf_upswd.read_only = False
-                eb_confirmar_login.disabled = False
-                pagina.update()
+                print(F"ERRO : {r.status_code}")
 
     def tela_sucesso_login():
         pop_up_2fl.open = False
@@ -55,6 +73,7 @@ def main(pagina:Page):
         pagina.update()
     
     def tela_home(evento):
+        pop_up_sucesso_cadastro.open = False
         pagina.remove_at(0)
         pagina.add(col_home)
         pagina.update()
@@ -64,22 +83,67 @@ def main(pagina:Page):
         pagina.update()
 
     def testar_cod(evento):
-        el_mail = tf_elogin.value
+        el_mail = eval( requests.get(f"{os.environ['link_api_login']}{tf_elogin.value},{tf_upswd.value}").text ).get('email')
         el_codigo = tf_cod2fl.value
         if el_codigo != "" and len(el_codigo) == 5:
-            r_api_2f = requests.get(f"{os.environ['b4']}{el_mail},{el_codigo}").text ###################
-            if r_api_2f == 'True':
-                tela_sucesso_login()
-            else: 
-                pop_up_2fl.title = ft.Text('COD ERRADO')
+            try:
+                r_api_2f = requests.get(f"{os.environ['b4']}{el_mail},{el_codigo}").text ###################
+                if r_api_2f == 'True':
+                    tela_sucesso_login()
+                else: 
+                    pop_up_2fl.title = ft.Text('COD ERRADO')
+                    pagina.update()
+            except:
+                print("CAIU NO EXCEPT DO TESTAR COD")
+
+    def confirmar_registro(e):
+        if tf_email.value != "" and tf_pswd.value != "" and tf_pswd2.value != "":
+            tf_email.disabled = True
+            tf_pswd.disabled = True
+            tf_pswd2.disabled = True
+            pagina.update()
+            rte = requests.get(f"{os.environ['b5']}{tf_email.value}").text 
+            if rte == '{"False": "False"}':
+                print("Email nao cadastrado no sistema")
+                requests.get(f"{os.environ['b6']}/{tf_email.value},{tf_pswd.value}") 
+                tf_email.disabled = False
+                tf_pswd.disabled = False
+                tf_pswd2.disabled = False
+                pagina.dialog = pop_up_sucesso_cadastro
+                pop_up_sucesso_cadastro.open = True
+                pagina.update()
+            elif rte == '{"True": "True"}':
+                print("Email no sistema")
+                tf_email.disabled = False
+                tf_pswd.disabled = False
+                tf_pswd2.disabled = False
+                pop_up_ja_cadastrado.open = True
+                pagina.dialog = pop_up_ja_cadastrado
+                pagina.update()
+            elif rte == '{"erro": "bd"}':
+                tf_email.disabled = False
+                tf_pswd.disabled = False
+                tf_pswd2.disabled = False
+                print("BD OFF KKKKK")
+                pagina.update()
+            else:
+                tf_email.disabled = False
+                tf_pswd.disabled = False
+                tf_pswd2.disabled = False
+                print("N SEI Q ERRO DEU MAS DEU ERRO")
                 pagina.update()
 
+    def fechar_pop_up_emailexistente(e):
+        pop_up_ja_cadastrado.open = False
+        pagina.update()
 
-
+    #CENTRALIZAR 
     pagina.vertical_alignment = ft.MainAxisAlignment.CENTER
     pagina.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
+    #REUTILIZAVEL
     eb_home = ft.ElevatedButton(icon=ft.icons.ARROW_BACK, text='VOLTAR', on_click=tela_home)
+
     #PAG INICIAL
     eb_login = ft.ElevatedButton(text='LOGIN', on_click=tela_login)
     eb_registrar = ft.ElevatedButton(text='REGISTRAR', on_click=tela_registrar)
@@ -87,24 +151,27 @@ def main(pagina:Page):
     linha_bemvindo = ft.Row([txt_bemvindo], alignment=ft.MainAxisAlignment.CENTER, spacing=1000)
     linha_bts = ft.Row([eb_login,eb_registrar],alignment=ft.MainAxisAlignment.CENTER, spacing=10)
     col_home = ft.Column(controls=[linha_bemvindo,linha_bts])
+
     #PAG REGISTRO
     tf_email = ft.TextField(label='Email', width=300)
     tf_pswd = ft.TextField(label='Senha',password=True, can_reveal_password=True, width=300)
     tf_pswd2 = ft.TextField(label='Confirme a senha',password=True, can_reveal_password=True, width=300)
-    eb_confirmar_registrar = ft.ElevatedButton(text='CONFIRMAR')
+    eb_confirmar_registrar = ft.ElevatedButton(text='CONFIRMAR', on_click=confirmar_registro)
     linha_bts_registro = ft.Row([eb_home,eb_confirmar_registrar], alignment=ft.MainAxisAlignment.CENTER)
     col_registro = ft.Column(controls=[tf_email,tf_pswd,tf_pswd2,linha_bts_registro], horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+
     #PAG LOGIN
     tf_elogin = ft.TextField(label='Login', width=300)
     tf_upswd = ft.TextField(label='Senha', password=True, can_reveal_password=True, width=300)
     eb_confirmar_login = ft.ElevatedButton(text='LOGAR', on_click=logar)
     linha_btn_login = ft.Row([eb_home,eb_confirmar_login],alignment=ft.MainAxisAlignment.CENTER)
     col_login = ft.Column(controls=[tf_elogin,tf_upswd, linha_btn_login], horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+   
     #PAG LOGADO
     txt_logado = ft.Text(value='LOGADO COM SUCESSO')
 
     #POP UP PROBLEMA NO SERVIDOR
-    pop_up_erroserv = ft.AlertDialog( open=False, modal=True, title=ft.Text(value='FALHA NO SERVIDOR'), actions=[ft.ElevatedButton("OK", on_click=fpop_up_erros)])
+    pop_up_erroserv = ft.AlertDialog( open=False, modal=True, title=ft.Text(value='DADOS NAO CONFEREM'), actions=[ft.ElevatedButton("OK", on_click=fpop_up_erros)])
 
     #POP UP 2FACTOR LOGIN
     tf_cod2fl = ft .TextField(label="COD", keyboard_type=ft.KeyboardType.NUMBER, max_length=5)
@@ -115,9 +182,24 @@ def main(pagina:Page):
         content=tf_cod2fl, 
         actions=[ ft.ElevatedButton("OK", on_click=testar_cod) ]
         )
-
+    
+    #POP UP DE EMAIL JA CADASTRADO
+    pop_up_ja_cadastrado = ft.AlertDialog(
+        open=False,
+        modal=True,
+        title=ft.Text('Ja existe uma conta associada a este email'),
+        actions=[ft.ElevatedButton('Ok', on_click=fechar_pop_up_emailexistente )]
+    )
+    #POP UP SUCESSO DE CADASTRO
+    pop_up_sucesso_cadastro = ft.AlertDialog(
+        open=False,
+        modal=False,
+        title=ft.Text('Cadastro realizado com sucesso, redirecionando para tela principal...'),
+        actions=[ft.ElevatedButton('Ok', on_click=tela_home )]
+    )
+    
     pagina.add(col_home)
 
     
 
-ft.app(target=main) #, view=ft.WEB_BROWSER
+ft.app(target=main) # view=ft.WEB_BROWSER
